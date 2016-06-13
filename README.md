0926536fe8de76a75af5979652b8d00b3edc5910
msrethab <msrethab@gmail.com>
2016-06-12 23:46:04 -0700

    Version 1.0.1

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Version 1.0.0
 -Included Change calculation functionality
 
 -Applied default Bootstrap styles to match mock-up
+
+
+Version 1.0.1
+
+
+-Added error message for too little money tendered
--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-
 //Defining Function for Button on Click event
 
-$(document).ready(function(){
-	$('#button').on('click', function(){
+$(document).ready(function() {
+    $('#button').on('click', function() {
 
-	//Calling user inputs from document
-	var amtSale = $("#amtSale").val();
-	var amtTender = $("#amtTender").val();
+            //Calling user inputs from document
+            var amtSale = $("#amtSale").val();
+            var amtTender = $("#amtTender").val();
 
-	//Applying appropriate formulas to calculate change
-	var totalChange = amtTender - amtSale;
-	var numDollar = Math.floor(totalChange);
-	var remainChange = totalChange - numDollar;
+            if (amtSale > amtTender) {
+                alert("You still owe money! Please check the amount tendered!");
 
-	var numQuarter = Math.floor(remainChange/0.25);
-	remainChange = remainChange % 0.25;
-	var numDime = Math.floor(remainChange / 0.10);
-	remainChange = remainChange % 0.10;
-	var numNickel = Math.floor(remainChange / 0.05);
-	remainChange = remainChange % 0.05;
-	var numPenny = Math.floor(remainChange / 0.01);
+            } else {
 
-	//Outputting the change to the document
+                //Applying appropriate formulas to calculate change
+                
+                var totalChange = amtTender - amtSale;
+                var numDollar = Math.floor(totalChange);
+                var remainChange = totalChange - numDollar;
 
-	$('.numDollar').html(numDollar);
-	$('.numQuarter').html(numQuarter);
-	$('.numDime').html(numDime);
-	$('.numNickel').html(numNickel);
-	$('.numPenny').html(numPenny);
+                var numQuarter = Math.floor(remainChange / 0.25);
+                remainChange = remainChange % 0.25;
+                var numDime = Math.floor(remainChange / 0.10);
+                remainChange = remainChange % 0.10;
+                var numNickel = Math.floor(remainChange / 0.05);
+                remainChange = remainChange % 0.05;
+                var numPenny = Math.floor(remainChange / 0.01);
 
-	})
+                //Outputting the change to the document
+
+                $('.numDollar').html(numDollar);
+                $('.numQuarter').html(numQuarter);
+                $('.numDime').html(numDime);
+                $('.numNickel').html(numNickel);
+                $('.numPenny').html(numPenny);
+            }
+    })
 });
